84f6e4018cf11836160c460e13a1358dc19f7a0b
Mabi19 <mateuszbartnik33@gmail.com>
2022-12-28 12:33:54 +0100

    attempt 3 at CD
    i am very dumb and stupid

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  worker: {
-    format: "iife", // this is needed for Firefox
-  }
+    plugins: [
+        vue(),
+    ],
+    worker: {
+        format: "iife", // this is needed for Firefox
+    },
+    base: "/luckalyzer/"
 });
