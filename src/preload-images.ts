0522ed6e7205cd00b0cf4c5cc20f92d6e2461e50
Mabi19 <mateuszbartnik33@gmail.com>
2023-01-28 16:31:36 +0100

    Make image preloading smarter

--- src/preload-images.ts
+++ src/preload-images.ts
@@ -1,12 +1,14 @@
 import { imageURLs } from "./Background";
 
-let preloadedBackgrounds;
 // preload the images
-// we need to keep these in memory, so they're going in this variable
+// this allows for instant background switching
+// at the expense of longer load times
 export const preload = () => {
-    preloadedBackgrounds = Object.values(imageURLs).map((url) => {
-        const img = new Image();
-        img.src = url;
-        return img;
+    Object.values(imageURLs).forEach((url) => {
+        const preloadTag = document.createElement("link");
+        preloadTag.rel = "preload";
+        preloadTag.as = "image";
+        preloadTag.href = url;
+        document.head.appendChild(preloadTag);
     });
 }
