d34856ff39e43c74d5bbc98326e1a75163a3ea40
Mabi19 <mateuszbartnik33@gmail.com>
2023-01-30 13:14:12 +0100

    Change image preloading again

--- src/preload-images.ts
+++ src/preload-images.ts
@@ -1,14 +1,26 @@
 import { imageURLs } from "./Background";
+const startingBackground = imageURLs.generic;
+
+// technically, the images are cached,
+// but store them just in case
+const images = [];
 
 // preload the images
 // this allows for instant background switching
 // at the expense of longer load times
 export const preload = () => {
-    Object.values(imageURLs).forEach((url) => {
-        const preloadTag = document.createElement("link");
-        preloadTag.rel = "preload";
-        preloadTag.as = "image";
-        preloadTag.href = url;
-        document.head.appendChild(preloadTag);
-    });
+    // only load the other ones when the starting one loads
+    // (so they won't slow down the starting one)
+    const startingImage = new Image();
+    startingImage.src = startingBackground;
+    startingImage.addEventListener("load", () => {
+        images.push(startingImage);
+        // we've already loaded the starting image
+        Object.values(imageURLs).filter((url) => url != startingBackground).forEach((url) => {
+            const image = new Image();
+            image.src = url;
+            // we need to store them somewhere
+            images.push(image);
+        });
+    })
 }
